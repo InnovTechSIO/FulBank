1bbfd0ef546a70630a5d81a53e61e9d107b69034
Molky <32463747+Molky-dev@users.noreply.github.com>
2023-09-18 14:33:28 +0200

    ddd

---
+++ src/main/java/fr/innovtech/fulbank/controller/Controller.java
@@ -0,0 +1,63 @@
+package fr.innovtech.fulbank.controller;
+
+
+import fr.innovtech.fulbank.App;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import de.jensd.fx.glyphs.fontawesome.FontAwesomeIcon;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Controller {
+
+
+    private double xOffset = 0;
+    private double yOffset = 0;
+
+
+    @FXML
+    private Rectangle topPane;
+
+
+    @FXML
+    protected void handleClickAction(MouseEvent event) {
+        xOffset = event.getScreenX() - (topPane.getScene().getWindow()).getX();
+        yOffset = event.getScreenY() - (topPane.getScene().getWindow()).getY();
+    }
+
+
+    @FXML
+    protected void switchSceneAccount(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("account.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1569, 970);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    @FXML
+    protected void switchToConnexionView(MouseEvent event) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1569, 970);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void handleMovementAction(MouseEvent event) {
+        Stage stage = (Stage) topPane.getScene().getWindow();
+        stage.setX(event.getScreenX() - xOffset);
+        stage.setY(event.getScreenY() - yOffset);
+    }
+
+
+}
