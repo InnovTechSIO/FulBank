b32261ea3c97a417456e8d591a70c304112e1389
Matis GASSE <matis.gasse@lyceefulbert.fr>
2023-10-03 11:15:50 +0200

    add beneficiary

--- src/main/java/fr/innovtech/fulbank/controller/MainController.java
+++ src/main/java/fr/innovtech/fulbank/controller/MainController.java
@@ -18,14 +18,24 @@ public class MainController implements Initializable {
     @FXML
     protected void switchProfile(MouseEvent event) throws IOException {
 
-
-
         FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("profile.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1569, 970);
         Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
         stage.setScene(scene);
         stage.show();
     }
+
+    @FXML
+    protected void switchPayment(MouseEvent event) throws IOException {
+
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("payment.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1569, 970);
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
 
 
     @Override
