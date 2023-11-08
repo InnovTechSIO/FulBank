db87418e6aef56f2037a35455742a2665aaccc4a
Matis GASSE <matis.gasse@lyceefulbert.fr>
2023-11-08 13:54:19 +0100

    ajout

--- src/main/java/fr/innovtech/fulbank/controller/ViewController/PaymentViewController.java
+++ src/main/java/fr/innovtech/fulbank/controller/ViewController/PaymentViewController.java
@@ -100,7 +100,7 @@ public class PaymentViewController extends ViewController implements Initializab
         int amount = parseInt(txt_montant.getCharacters().toString());
         String accountAdd = cbx_vers.getValue().toString();
         String accountSubstract = cbx_depuis.getValue().toString();
-        AccountDBController.Payment(amount, idCustomer, accountAdd, accountSubstract, idCustomer2);
+        AccountDBController.Payment(amount, idCustomer, accountAdd, accountSubstract);
     }
 
 
