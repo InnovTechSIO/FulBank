343453d413ed1bf4f835a7963cefc9853597d078
Matis GASSE <matis.gasse@lyceefulbert.fr>
2023-12-13 14:44:03 +0100

    nul

--- src/main/java/fr/innovtech/fulbank/controller/ViewController/AccountViewController.java
+++ src/main/java/fr/innovtech/fulbank/controller/ViewController/AccountViewController.java
@@ -2,11 +2,15 @@ package fr.innovtech.fulbank.controller.ViewController;
 
 import fr.innovtech.fulbank.controller.DBController.AccountDBController;
 import fr.innovtech.fulbank.controller.DBController.CustomerDBController;
+import fr.innovtech.fulbank.entities.Account;
+import fr.innovtech.fulbank.entities.Customer;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
+import javafx.scene.shape.Rectangle;
 
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class AccountViewController extends ViewController implements Initializable {
@@ -14,10 +18,16 @@ public class AccountViewController extends ViewController implements Initializab
     @FXML
     private Label lbl_indiv;
 
+
     @FXML
     protected void setLbl_indiv(){
         float amount = AccountDBController.getAmount(CustomerDBController.connectedCustomer.get_id(), "courant");
         lbl_indiv.setText(String.valueOf(amount));
+    }
+
+    @FXML
+    protected void setCourants(){
+        //ArrayList<> accounts = AccountDBController.getAllAccountsByCustomer(CustomerDBController.connectedCustomer.get_id());
     }
 
     @Override
